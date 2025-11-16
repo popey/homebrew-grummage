e172a833c1c322d26f2f1c57f1a530b7f1b4b5f8
Alan Pope <alan@popey.com>
2025-11-16 19:48:54 +0000

    Add missing linkify dependencies for textual
    Fixes ModuleNotFoundError: Linkify enabled but not installed
    
    Textual requires markdown-it-py with linkify extras, which needs:
    - linkify-it-py
    - mdit-py-plugins  
    - uc-micro-py

--- Formula/grummage.rb
+++ Formula/grummage.rb
@@ -43,6 +43,21 @@ class Grummage < Formula
     url "https://files.pythonhosted.org/packages/source/p/pygments/pygments-2.19.2.tar.gz"
     sha256 "636cb2477cec7f8952536970bc533bc43743542f70392ae026374600add5b887"
   end
+
+  resource "linkify-it-py" do
+    url "https://files.pythonhosted.org/packages/2a/ae/bb56c6828e4797ba5a4821eec7c43b8bf40f69cda4d4f5f8c8a2810ec96a/linkify-it-py-2.0.3.tar.gz"
+    sha256 "68cda27e162e9215c17d786649d1da0021a451bdc436ef9e0fa0ba5234b9b048"
+  end
+
+  resource "mdit-py-plugins" do
+    url "https://files.pythonhosted.org/packages/b2/fd/a756d36c0bfba5f6e39a1cdbdbfdd448dc02692467d83816dff4592a1ebc/mdit_py_plugins-0.5.0.tar.gz"
+    sha256 "f4918cb50119f50446560513a8e311d574ff6aaed72606ddae6d35716fe809c6"
+  end
+
+  resource "uc-micro-py" do
+    url "https://files.pythonhosted.org/packages/91/7a/146a99696aee0609e3712f2b44c6274566bc368dfe8375191278045186b8/uc-micro-py-1.0.3.tar.gz"
+    sha256 "d321b92cff673ec58027c04015fcaa8bb1e005478643ff4a500882eaab88c48a"
+  end
     
   def install
     python3 = "python3.11"
